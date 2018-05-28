d8383d96943bbbc41b0fa326bde3e07f5f12e2be
huihui <glorydom@163.com>
2018-05-28 23:44:10 +0800

    修改页面，表格转图片为jpeg格式

--- article/js/article_modi.js
+++ article/js/article_modi.js
@@ -32,7 +32,7 @@ function domToimage(table) {
         quality: 1.0,
         bgcolor: '#fff',
         width: $(table).width()
-    }.then(function (dataUrl) {
+    }).then(function (dataUrl) {
             return sumitImageFile(dataUrl);
         }).then(function (src) {
             // console.log(src);
@@ -50,7 +50,7 @@ function domToimage(table) {
             node$.remove();
             console.error('转换图片出错，可能遇到跨域问题,但仍可继续操作!', error);
             deferred.reject(error);
-        }))
+        });
     return deferred.promise();
 }
 
